a941685e1cb0686d857665b3b849dd0b86ff7240
boxdate <88315712+boxdate@users.noreply.github.com>
2024-02-13 20:55:18 +0900

    add ReqJapanese.py
    日本語実行プログラムを追加

---
+++ Designing/doc/JapaneseProgramming/ReqJapanese.py
@@ -0,0 +1,13 @@
+Arduino上のボタンを = input('ボタンが押したか押していないかを入力してください（押した/押していない）: ')
+Arduino上のLEDは = print
+
+実際に Arduino上のボタンを == '押した' とき
+    Arduino上のLEDは('ボタンを検知する')
+    
+    実際に Arduino上のボタンを == '押した' とき
+        Arduino上のLEDは('点灯する')
+    でなければ
+        Arduino上のLEDは('何もしない')
+        
+でなければ
+    Arduino上のLEDは('何もしない')
